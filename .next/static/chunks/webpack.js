9889ca038f4a902954302fe78aa3f26359e878ef
Shawn J <shontek@gmail.com>
2021-11-28 21:17:37 -0800

    Remove background color from grid divs

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "32e531cb27fb57c80a0d";
+/******/ 	var hotCurrentHash = "bfd745433bcd33dcb270";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- src/components/Grid/index.jsx
+++ src/components/Grid/index.jsx
@@ -1,13 +1,15 @@
 import styled from 'styled-components';
 
+// Grid styling borrowed from https://css-tricks.com/a-grid-of-logos-in-squares/
+
 const Grid = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
   grid-gap: 0;
 
   & > div {
-    background: black;
-    padding: 1rem;
+    // background: black;
+    // padding: 1rem;
     position: relative;
 
     &::before {
@@ -20,13 +22,6 @@ const Grid = styled.div`
     img {
         position: absolute;
         max-width: 100%;
-  
-        // Alternate Version
-        // top: 0;
-        // bottom: 0;
-        // right: 0;
-        // left: 0;
-        // margin: auto;
   
         top: 50%;
         left: 50%;
