a13d74ea89ad45664e2eb23aa392eeca005a1e52
Shawn J <shontek@gmail.com>
2021-12-02 19:04:54 -0800

    Uninstall theme-ui library since we use styled-components
    Remove unused notes and temp folder and files
    
    Use styled components for Spinner styling
    
    Break out GlobalStyle into its own component

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,19 +1,7 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { ThemeProvider } from 'styled-components'
 import Nav from '../src/components/Nav';
+import GlobalStyle from '../src/assets/styles';
 import { StateProvider } from '../src/providers/store';
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: 'Inter', Helvetica, Arial, sans-serif;
-  }
-
-  p, h1, h2, h3, h4, h5, h6 {
-    line-height: 1.4rem;
-  }
-`
 
 const theme = {
   colors: {
@@ -34,5 +22,5 @@ export default function App({ Component, pageProps }) {
         </StateProvider>
       </ThemeProvider>
     </>
-  )
+  );
 }
