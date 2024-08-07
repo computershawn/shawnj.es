62ca551c05fdbfa24e885932d4f32b6352e4670c
computershawn <shontek@gmail.com>
2024-08-07 13:42:08 -0700

    Install Chakra Use Chakra Box and Flex components for Card

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,7 @@
 import { ThemeProvider } from 'styled-components'
+
+import { ChakraProvider } from '@chakra-ui/react'
+
 import Nav from '../src/components/Nav';
 import GlobalStyle from '../src/assets/styles';
 import { StateProvider } from '../src/providers/store';
@@ -13,14 +16,14 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <StateProvider>
-          {/* <main> */}
-          <Nav />
-          <Component {...pageProps} />
-          {/* </main> */}
-        </StateProvider>
-      </ThemeProvider>
+      <ChakraProvider>
+        <ThemeProvider theme={theme}>
+          <StateProvider>
+            <Nav />
+            <Component {...pageProps} />
+          </StateProvider>
+        </ThemeProvider>
+      </ChakraProvider>
     </>
   );
 }
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,28 +1,6 @@
 import React from 'react';
 import Link from 'next/link'
-import styled from 'styled-components';
-
-const StyledCard = styled.div`
-  div {
-    background: rgb(0, 0, 0, 0.8);
-    color: #fefefe;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    z-index: 2;
-    opacity: 0;
-    transition: opacity 300ms;
-
-    &:hover {
-      opacity: 1;
-    }
-  }
-`;
+import { Box, Flex } from '@chakra-ui/react';
 
 const captionStyle = {
   margin: '0 1.5rem',
@@ -33,17 +11,45 @@ const Card = ({ proj }) => {
   const { thumbnail, title, slug } = proj;
 
   return (
-    <StyledCard>
+    <Box>
       <Link href="[slug]" as={`${slug}`} passHref>
         <>
-          <div>
+          <Flex
+            background='rgb(0, 0, 0, 0.8)'
+            color='#fefefe'
+            display='flex'
+            justify='center'
+            align='center'
+            position='absolute'
+            top={0}
+            left={0}
+            width='100%'
+            height='100%'
+            zIndex={2}
+            opacity={0}
+            transition='opacity 300ms'
+            _hover={{
+              opacity: 1,
+            }}
+          >
             <caption style={captionStyle}>{title}</caption>
-          </div>
+          </Flex>
           <img src={thumbnail} alt={title} />
         </>
       </Link>
-    </StyledCard>
+    </Box>
   );
 };
 
 export default Card;
+
+// Use of Link should probably be more like this:
+//
+// import NextLink from 'next/link'
+// import { Link } from '@chakra-ui/react'
+
+// <Link as={NextLink} href='/home'>
+//   Home
+// </Link>
+//
+// Ref: https://v2.chakra-ui.com/docs/components/link
