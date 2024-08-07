3192e395537b47c73395fa372b35bb2150f11aed
computershawn <shontek@gmail.com>
2024-08-06 23:44:19 -0700

    Add hover effect to card overlay

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,40 +1,48 @@
 import React from 'react';
 import Link from 'next/link'
+import styled from 'styled-components';
 
-const cardOverlayStyle = {
-  background: 'rgb(0, 0, 0, 0.36)',
-  color: '#fefefe',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  position: 'absolute',
-  top: 0,
-  left: 0,
-  width: '100%',
-  height: '100%',
-  zIndex: 2,
-}
+const StyledCard = styled.div`
+  div {
+    background: rgb(0, 0, 0, 0.8);
+    color: #fefefe;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 2;
+    opacity: 0;
+    transition: opacity 400ms;
+
+    &:hover {
+      opacity: 1;
+    }
+  }
+`;
 
 const captionStyle = {
   margin: '0 1.5rem',
   fontSize: '1.5rem',
 };
 
-// TODO: Add hover state CSS. The card's overlay
-// including text should fade in on hover
-
 const Card = ({ proj }) => {
   const { thumbnail, title, slug } = proj;
 
   return (
-    <div>
-      <div style={cardOverlayStyle}>
-        <caption style={captionStyle}>{title}</caption>
-      </div>
+    <StyledCard>
       <Link href="[slug]" as={`${slug}`} passHref>
-        <img src={thumbnail} alt={title} />
+        <>
+          <div>
+            <caption style={captionStyle}>{title}</caption>
+          </div>
+          <img src={thumbnail} alt={title} />
+        </>
       </Link>
-    </div>
+    </StyledCard>
   );
 };
 
