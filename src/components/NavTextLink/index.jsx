ca8b0bba562830dcbaf16c54cee1c5cc59c62d02
computershawn <shontek@gmail.com>
2024-08-08 23:15:51 -0700

    Remove styled components from NavTextLink

--- src/components/NavTextLink/index.jsx
+++ src/components/NavTextLink/index.jsx
@@ -1,23 +1,46 @@
 import React from 'react';
-import Link from 'next/link';
+import NextLink from 'next/link'
 
-import { NavTextLinkContainer } from './styles';
+import { Link } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 
 const NavTextLink = ({ text, href, newTab, cl }) => {
   const linkIsInternal = !href.startsWith('http');
   const t = newTab ? '_blank' : null;
   const linkContent = linkIsInternal ? (
-    <Link href={href}>
+    <Link as={NextLink} href={href}>
       <span className={cl}>{text}</span>
     </Link>
   ) : (
-    <a href={href} target={t}>{text}</a>
+    <Link as={NextLink} href={href} isExternal={newTab}>
+      {text}
+    </Link>
   );
 
   return (
-    <NavTextLinkContainer>
+    <Box
+      display="inline-block"
+      mr="2rem"
+      //   & > a {
+      //     text-decoration: none;
+      //     transition: color 200ms ease-in-out;
+
+      //     &:visited, active {
+      //       color: #8a8a8a;
+      //     }
+
+      //     &:hover, active {
+      //       color: #222;
+      //     }
+
+      //     &.current {
+      //       color: #222;
+      //     }
+      //   }
+      // `;
+    >
       {linkContent}
-    </NavTextLinkContainer>
+    </Box>
   );
 };
 
