5a9030c11c8f8e8b24429444943d632776c2c37a
computershawn <shontek@gmail.com>
2024-08-13 21:57:30 -0700

    Remove styled components from _app and _document Remove unneeded GlobalStyle

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,28 +1,16 @@
-import { ThemeProvider } from 'styled-components'
-
 import { ChakraProvider } from '@chakra-ui/react'
 
 import Nav from '../src/components/Nav';
-import GlobalStyle from '../src/assets/styles';
 import { StateProvider } from '../src/providers/store';
-
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
       <ChakraProvider>
-        <ThemeProvider theme={theme}>
-          <StateProvider>
-            <Nav />
-            <Component {...pageProps} />
-          </StateProvider>
-        </ThemeProvider>
+        <StateProvider>
+          <Nav />
+          <Component {...pageProps} />
+        </StateProvider>
       </ChakraProvider>
     </>
   );
