54d2e71c095a0fad777c64eab66ab910b552edd3
computershawn <shontek@gmail.com>
2024-08-30 15:42:49 -0700

    Add apple touch icon and other favicon sizes

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -11,23 +11,23 @@ export default function App({ Component, pageProps }) {
         <StateProvider>
           <Head>
             <link rel='shawnj icon' href='/images/favicon.ico' />
-            {/* <link
+            <link
               rel='apple-touch-icon'
               sizes='180x180'
-              href='/images/icon.ico'
-            /> */}
-            {/* <link
+              href='/images/apple-touch-icon.png'
+            />
+            <link
               rel='icon'
               type='image/png'
               sizes='32x32'
-              href='/images/icon.ico'
+              href='/images/favicon-32x32.png'
             />
             <link
               rel='icon'
               type='image/png'
               sizes='16x16'
-              href='/images/icon.ico'
-            /> */}
+              href='/images/favicon-16x16.png'
+            />
           </Head>
           <Nav />
           <Component {...pageProps} />
