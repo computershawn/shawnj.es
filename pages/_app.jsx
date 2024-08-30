d3a72ee26348f5915c622cfa4647ef57855e071b
computershawn <shontek@gmail.com>
2024-08-30 15:35:39 -0700

    Place icon inside _app

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,5 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react';
+import Head from 'next/head';
 
 import Nav from '../src/components/Nav';
 import { StateProvider } from '../src/providers/store';
@@ -8,6 +9,26 @@ export default function App({ Component, pageProps }) {
     <>
       <ChakraProvider>
         <StateProvider>
+          <Head>
+            <link rel='shawnj icon' href='/images/favicon.ico' />
+            {/* <link
+              rel='apple-touch-icon'
+              sizes='180x180'
+              href='/images/icon.ico'
+            /> */}
+            {/* <link
+              rel='icon'
+              type='image/png'
+              sizes='32x32'
+              href='/images/icon.ico'
+            />
+            <link
+              rel='icon'
+              type='image/png'
+              sizes='16x16'
+              href='/images/icon.ico'
+            /> */}
+          </Head>
           <Nav />
           <Component {...pageProps} />
         </StateProvider>
