4d937b42ab4bebda9c008f652d9a71ae158885a0
computershawn <shontek@gmail.com>
2026-03-10 16:16:04 -0700

    Remove SJ×MDP link Add link to my Instagram

--- src/components/CustomIcons/index.tsx
+++ src/components/CustomIcons/index.tsx
@@ -1,7 +1,15 @@
 import EmailIcon from './EmailIcon';
 import GitHubIcon from './GitHubIcon';
+import InstagramIcon from './InstagramIcon';
 import LinkedinIcon from './LinkedinIcon';
 import PlayIcon from './PlayIcon';
 import UnlinkIcon from './UnlinkIcon';
 
-export { EmailIcon, GitHubIcon, LinkedinIcon, PlayIcon, UnlinkIcon };
+export {
+  EmailIcon,
+  GitHubIcon,
+  InstagramIcon,
+  LinkedinIcon,
+  PlayIcon,
+  UnlinkIcon,
+};
